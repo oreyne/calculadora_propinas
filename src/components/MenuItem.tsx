82bc21858bd6adedc05c4c9396b14de88b252c3d
Oreyne Lang <oreynelang14@gmail.com>
2024-12-18 23:20:28 -0400

    restoring all files

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,16 @@
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+  item : MenuItem,
+  addItem: () => void
+}
+
+export default function MenuItem({ item } : MenuItemProps) {
+  return (
+    <button
+      className="border-2 border-teal-400 hover:bg-teal-400 w-full p-3 flex justify-between">
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  )
+}
